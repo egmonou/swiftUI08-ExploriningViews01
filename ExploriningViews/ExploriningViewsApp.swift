3491911dea6814fd7a51ae42ea1a51fa378b8336
Nour <mohamednour25@gmail.com>
2024-09-09 01:46:22 +0300

    Initial Commit

---
+++ ExploriningViews/ExploriningViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploriningViewsApp.swift
+//  ExploriningViews
+//
+//  Created by administrator on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExploriningViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
